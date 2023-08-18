ebfcf79fedef4d67294abc78eb9f4eecf8d1e9ef
TalhaBayansar <talha.bayansar@ordina.be>
2023-08-19 01:41:38 +0200

    test

--- src/movies/containers/MovieSection.tsx
+++ src/movies/containers/MovieSection.tsx
@@ -6,8 +6,8 @@ import { Star } from "~/shared/icons";
 
 type Props = {
   title: string;
-  movies: Movie[];
-  genres: Genre[];
+  movies?: Movie[];
+  genres?: Genre[];
 };
 
 export const MovieSection = component$(({ title, movies, genres }: Props) => {
@@ -15,7 +15,7 @@ export const MovieSection = component$(({ title, movies, genres }: Props) => {
     <div class="flex flex-col">
       <SubTitle class="mb-4">{title}</SubTitle>
       <div class="flex gap-4 overflow-x-scroll">
-        {movies.map((movie) => (
+        {movies?.map((movie) => (
           <MovieCard key={`now_playing_${movie.id}`} movie={movie}>
             <div class="flex items-center gap-1 text-sm" q:slot="subtitle">
               <Star size={12} /> {movie.vote_average}
@@ -25,7 +25,7 @@ export const MovieSection = component$(({ title, movies, genres }: Props) => {
               q:slot="footer"
             >
               {genres
-                .filter((genre) => movie.genre_ids.includes(genre.id))
+                ?.filter((genre) => movie.genre_ids.includes(genre.id))
                 .map((genre) => genre.name)
                 .join(" | ")}
             </div>
