e4faed015104320166c51563561fb7b5df57da47
TalhaBayansar <talha.bayansar@ordina.be>
2023-09-13 09:36:47 +0200

    adding fallback states for searched movies

--- src/routes/(root)/search/index.tsx
+++ src/routes/(root)/search/index.tsx
@@ -56,24 +56,34 @@ export default component$(() => {
           class="rounded-lg p-3 w-full text-c-background"
         />
       </Form>
-      <div class="grid grid-cols-2 gap-8 place-content-center overflow-hidden">
-        {movies.value?.movie.results.map((movie) => (
-          <MovieCard key={movie.id} movie={movie}>
-            <div class="flex items-center gap-1 text-sm" q:slot="subtitle">
-              <Star size={12} /> {movie.vote_average.toFixed(1)}
-            </div>
-            <div
-              class="text-c-text-small min-w-0 max-w-full text-xs text-ellipsis whitespace-nowrap overflow-hidden"
-              q:slot="footer"
-            >
-              {movies.value.genre.genres
-                .filter((genre) => movie.genre_ids.includes(genre.id))
-                .map((genre) => genre.name)
-                .join(" | ")}
-            </div>
-          </MovieCard>
-        ))}
-      </div>
+      {!movies.value ? (
+        <div class="w-full h-full grid place-items-center">
+          Search your favorite movies.
+        </div>
+      ) : movies.value.movie.total_results > 0 ? (
+        <div class="grid md:grid-cols-4 grid-cols-2 gap-8 place-content-center overflow-hidden">
+          {movies.value.movie.results.map((movie) => (
+            <MovieCard key={movie.id} movie={movie}>
+              <div class="flex items-center gap-1 text-sm" q:slot="subtitle">
+                <Star size={12} /> {movie.vote_average.toFixed(1)}
+              </div>
+              <div
+                class="text-c-text-small min-w-0 max-w-full text-xs text-ellipsis whitespace-nowrap overflow-hidden"
+                q:slot="footer"
+              >
+                {movies.value.genre.genres
+                  .filter((genre) => movie.genre_ids.includes(genre.id))
+                  .map((genre) => genre.name)
+                  .join(" | ")}
+              </div>
+            </MovieCard>
+          ))}
+        </div>
+      ) : (
+        <div class="w-full h-full grid place-items-center">
+          There were no results for the given title.
+        </div>
+      )}
     </div>
   );
 });
