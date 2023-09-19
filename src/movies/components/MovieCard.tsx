b9ab33334adb0a93f2e660662ce41d5637d230b7
TalhaBayansar <talha.bayansar@ordina.be>
2023-09-19 14:02:18 +0200

    scaling image on movie cards

--- src/movies/components/MovieCard.tsx
+++ src/movies/components/MovieCard.tsx
@@ -16,7 +16,7 @@ export const MovieCard = component$(({ movie, class: className }: Props) => {
         class="w-full h-full object-cover rounded mb-3"
         width={160}
         height={240}
-        src={getMediaUrl("w200", movie.poster_path)}
+        src={getMediaUrl("w300", movie.poster_path)}
         alt={`Poster of the movie: ${movie.title}`}
       />
       <h3 class="block w-full shrink-0 whitespace-nowrap text-ellipsis overflow-hidden text-sm font-medium">
